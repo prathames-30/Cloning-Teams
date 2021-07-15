20e9d4d8ad7f1bdbf8a2e5fe8765217470e906dd
prathames-30 <prathameshmuley11032006@gmail.com>
2021-07-16 04:22:04 +0530

    correct commit which was committed to heroku but not in github

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -4,7 +4,7 @@ import Peer from 'simple-peer'
 
 const SocketContext = createContext();
 
-const socket = io('/')
+const socket = io('https://cloning-teams-app.herokuapp.com/')
 
 const ContextProvider = ({ children }) => {
     
